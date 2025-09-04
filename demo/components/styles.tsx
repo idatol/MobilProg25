5f39df438c16bc06197685a078d4ca6cb03949fe
Ida Tollaksen <ida.k.tollaksen@gmail.com>
2025-09-04 09:57:13 +0200

    blllasjaksdas

---
+++ demo/components/styles.tsx
@@ -0,0 +1,13 @@
+const cardWidth = "70%";
+
+const ProductCardStyles = {
+    width: cardWidth,
+    backgroundColor: "White",
+    borderRadius: 10,
+    overflow: "hidden",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+    elevation: 5
+} as const;
