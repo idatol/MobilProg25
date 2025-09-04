aa66bcbb1320a2637459a04f2af3bc15b7d08fc1
Ida Tollaksen <ida.k.tollaksen@gmail.com>
2025-09-04 09:39:14 +0200

    minor changes

--- demo/App.tsx
+++ demo/App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ProductCard from './components/ProductCard';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ProductCard />
     </View>
   );
 }
---
+++ demo/components/ProductCard.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native"
+
+export default function ProductCard() {
+    return(
+        <View>
+            <Text>Product Name</Text>
+            <Text>Product Price</Text>
+            <Text>Product Description</Text>
+        </View>
+    )
+}
