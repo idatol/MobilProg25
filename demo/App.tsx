9899fbdace13d1068244bb57203c00a65ba052e1
Ida Tollaksen <ida.k.tollaksen@gmail.com>
2025-09-09 13:07:20 +0200

    fortsatt, korigert med forelesers kode

--- demo/App.tsx
+++ demo/App.tsx
@@ -1,6 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import ProductCard from './components/ProductCard';
+import { StyleSheet, View } from "react-native";
+import { ProductCard } from "./components/ProductCard";
 
 export default function App() {
   return (
@@ -13,8 +12,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- demo/components/ProductCard.tsx
+++ demo/components/ProductCard.tsx
@@ -1,11 +1,18 @@
-import { View, Text } from "react-native"
+import { View } from "react-native";
+import { productStyles } from "./styles";
+import { Image } from "expo-image";
+import ProductName from "./ProductName";
+import ProductPrice from "./ProductPrice";
+import BuyButton from "./BuyButton";
+const image = require("../assets/image.png");
 
-export default function ProductCard() {
-    return(
-        <View>
-            <Text>Product Name</Text>
-            <Text>Product Price</Text>
-            <Text>Product Description</Text>
-        </View>
-    )
-}
+export const ProductCard = () => {
+  return (
+    <View style={productStyles.card}>
+      <ProductName />
+      <ProductPrice />
+      <Image source={image} style={productStyles.image} contentFit="cover" />
+      <BuyButton />
+    </View>
+  );
+};
