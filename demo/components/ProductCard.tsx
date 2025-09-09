040ec888bc37bb1d3576278fd5b04ca93c0d6083
Ida Tollaksen <ida.k.tollaksen@gmail.com>
2025-09-09 14:00:43 +0200

    jgdf

--- demo/components/ProductCard.tsx
+++ demo/components/ProductCard.tsx
@@ -4,14 +4,14 @@ import { Image } from "expo-image";
 import ProductName from "./ProductName";
 import ProductPrice from "./ProductPrice";
 import BuyButton from "./BuyButton";
-const image = require("../assets/image.png");
+const image = "../assets/image.png";
 
 export const ProductCard = () => {
   return (
     <View style={productStyles.card}>
       <ProductName />
       <ProductPrice />
-      <Image source={image} style={productStyles.image} contentFit="cover" />
+      <Image source={image} style={{ width: 200, height: 200 }} />
       <BuyButton />
     </View>
   );
